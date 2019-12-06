d6be7c9b4c57abc062b068b36a9df7f895568123
giuliohome <giuliohome@gmail.com>
2019-12-06 19:16:12 +0100

    Day 6

---
+++ Day6 - Part1.fs
@@ -0,0 +1,43 @@
+﻿open System
+open System.IO
+open System.Diagnostics
+
+type Orbit = {sun: string; earth:string} with
+    static member Parse (line:string) =
+        let stars = line.Split(')')
+        {sun = stars.[0]; earth = stars.[1]}
+
+let readOrbits (path:string) : Orbit[] =
+    use sw = new StreamReader (path)
+    [|
+        while (not sw.EndOfStream) do
+            yield Orbit.Parse(sw.ReadLine())
+    |]  
+
+
+let scenario (input: Orbit[]) : int  =
+    let start = (0, [|"COM"|], 0) 
+    List.unfold 
+        (fun (all: int , old_ext: string[], max: int) ->
+            let new_orbits = 
+                input
+                |> Array.filter(fun o -> old_ext |> Array.contains o.sun)
+                |> Array.map(fun o -> o.earth)
+            let count = new_orbits.Length * (max + 1)
+            if count = 0 then None
+            else Some (all + count, (all + count, new_orbits, max + 1))
+        ) start
+    |> List.last
+    
+
+[<EntryPoint>]
+let main _ =
+    let timer = new Stopwatch()
+    timer.Start()
+    let input = readOrbits @"C:\dev\FSharp\AoC2019\Day6\input.txt"
+    let output = scenario input 
+    printfn "The answer is: %d" output
+    timer.Stop()
+    printfn "done in (%d) ms !" timer.ElapsedMilliseconds
+    Console.ReadKey() |> ignore
+    0    
---
+++ Day6 - Part2.fs
@@ -0,0 +1,54 @@
+﻿open System
+open System.IO
+open System.Diagnostics
+
+type Orbit = {sun: string; earth:string} with
+    static member Parse (line:string) =
+        let stars = line.Split(')')
+        {sun = stars.[0]; earth = stars.[1]}
+
+let readOrbits (path:string) : Orbit[] =
+    use sw = new StreamReader (path)
+    [|
+        while (not sw.EndOfStream) do
+            yield Orbit.Parse(sw.ReadLine())
+    |]  
+
+
+let scenario (input: Orbit[]) : int  =
+    let goal = input |> Array.find (fun o -> o.earth = "SAN")
+    let you = input |> Array.find (fun o -> o.earth = "YOU") 
+    let start = (0, [|you.sun|], [|"YOU"|]) 
+    List.unfold 
+        (fun (min: int , old_ext: string[], old_visited: string[]) ->
+            if old_ext |> Array.contains goal.sun then None else
+            let new_orbits_earth = 
+                input
+                |> Array.filter(fun o -> old_ext |> Array.contains o.sun)
+                |> Array.map(fun o -> o.earth)
+                |> Array.except(old_visited)
+            let new_orbits_sun = 
+                input
+                |> Array.filter(fun o -> old_ext |> Array.contains o.earth)
+                |> Array.map(fun o -> o.sun)
+                |> Array.except(old_visited)
+            let new_orbits = 
+                Array.append new_orbits_earth new_orbits_sun
+                |> Array.distinct
+            let new_visited = Array.append old_ext old_visited |> Array.distinct 
+            Some (min + 1, (min + 1, new_orbits, new_visited))
+        ) start
+    |> List.last
+    
+
+[<EntryPoint>]
+let main _ =
+    let timer = new Stopwatch()
+    timer.Start()
+    let input = readOrbits @"C:\dev\FSharp\AoC2019\Day6\input.txt"
+    let output = scenario input 
+    printfn "The answer is: %d" output
+    timer.Stop()
+    printfn "done in (%d) ms !" timer.ElapsedMilliseconds
+    Console.ReadKey() |> ignore
+    0    
