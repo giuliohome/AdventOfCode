deb38fc8b067df060c7398e924f458d01fa6acf3
giuliohome <giuliohome@gmail.com>
2019-12-08 06:49:45 +0100

    Day 8

---
+++ Day8 - Part 1.fs
@@ -0,0 +1,47 @@
+﻿open System
+open System.IO
+open System.Diagnostics
+
+let readInput (path:string)  = 
+    [|
+    
+        use sw = new StreamReader (path)
+        while (not sw.EndOfStream) do
+        yield [|
+            for i in [0..5] do
+            let layer = Array.init 25 (fun _ -> ' ') 
+            sw.ReadBlock(layer,0,25)
+            yield layer
+        |]
+        
+
+    |]
+
+let GetNumof (x: char [] []) (c:Char) =
+    x
+    |> Array.map(fun y ->
+        y
+        |> Array.filter(fun z -> z = c)
+        |> Array.length
+    )
+    |> Array.sum
+
+[<EntryPoint>]
+let main _ =
+    let timer = new Stopwatch()
+    timer.Start()
+    let input = readInput @"C:\dev\FSharp\AoC2019\Day8\input.txt"
+    let info =
+        input
+        |> Array.take(input.Length - 1)
+        |> Array.map(fun x ->
+            (GetNumof x '0', GetNumof x '1', GetNumof x '2')
+        )
+        |> Array.minBy(fun (a,b,c) -> a)
+        |> fun (a,b,c) -> b*c
+
+    printfn "The answer is %d " info
+    timer.Stop()
+    printfn "done in (%d) ms !" timer.ElapsedMilliseconds
+    Console.ReadKey() |> ignore
+    0    
---
+++ Day8 - Part2.fs
@@ -0,0 +1,67 @@
+﻿open System
+open System.IO
+open System.Diagnostics
+
+let readInput (path:string)  = 
+    [|
+    
+        use sw = new StreamReader (path)
+        while (not sw.EndOfStream) do
+        yield [|
+            for i in [0..5] do
+            let layer = Array.init 25 (fun _ -> ' ') 
+            sw.ReadBlock(layer,0,25)
+            yield layer
+        |]
+        
+
+    |]
+
+let GetNumof (x: char [] []) (c:Char) =
+    x
+    |> Array.map(fun y ->
+        y
+        |> Array.filter(fun z -> z = c)
+        |> Array.length
+    )
+    |> Array.sum
+
+let compute (state: char [] [] ) (t: char [] [] ) : char [] []  =
+          [|
+            for x in [0..5] do
+                yield 
+                    [|
+                        for y in [0..24] do
+                            yield if state.[x].[y] = '2' then t.[x].[y] else state.[x].[y]
+                    |]
+          |]  
+
+[<EntryPoint>]
+let main _ =
+    let timer = new Stopwatch()
+    timer.Start()
+    let input = readInput @"C:\dev\FSharp\AoC2019\Day8\input.txt"
+    let info =
+        input
+        |> Array.take(input.Length - 1)
+        |> Array.fold (fun state t ->
+            compute state t
+            )
+            ([|
+                for i in [0..5] do
+                let layer = Array.init 25 (fun _ -> '2') 
+                yield layer
+            |])
+
+
+    printfn "The answer is " 
+    info
+    |> Array.iter(fun x -> 
+        x 
+        |> Array.iter(fun y -> printf "%c" (if y = '0' then 'X' else ' ')) 
+        printfn ""
+    )
+    timer.Stop()
+    printfn "done in (%d) ms !" timer.ElapsedMilliseconds
+    Console.ReadKey() |> ignore
+    0    
