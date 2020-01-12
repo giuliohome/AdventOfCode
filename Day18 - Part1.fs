54349287ccc5d697749fef8af966026e237893c0
giuliohome <giuliohome@gmail.com>
2020-01-12 16:53:45 +0100

    find tree branches

--- Day18 - Part1.fs
+++ Day18 - Part1.fs
@@ -24,14 +24,95 @@ let findStart (map: char [] []) =
         |> Array.findIndex ((=) '@')
     (i1,i2)
 
+let getRect<'a> (map: 'a [][]) =
+    (map.[0].Length - 1, map.Length - 1)
+
 type Branch = {area: char; distance: int; position: (int * int)}
-type Explore = {branches: Branch[]; visited: (int * int) []}
+type Explore = {branches: Branch[]; visited: (int * int) list}
 
-let findBranches (map: char [] []) (visited: (int * int)[]) (position: int * int) 
+let getMoves (x:int,y:int) (xmax, ymax) (visited: (int * int) list) (map: char [][]) =
+    let mutable additional_visited = visited
+    let mutable N,S,E,W = false, false, false, false
+    let mutable yN, yS, xE, xW = y + 1, y - 1, x + 1, x - 1
+    let branches = 
+        [|  
+            //east
+            while (not E && xE < xmax ) do
+                if additional_visited |> List.contains (xE,y)
+                then E <- true else 
+                additional_visited <- (xE,y) :: additional_visited
+                match map.[xE].[y] with
+                | '.' ->
+                    xE <- xE + 1
+                | '#' -> 
+                    E <- true
+                | area ->
+                    E <- true
+                    yield {area = area; distance = xE - x; position = (xE, y)}
+            //to do west, north, sud
+        |]
+    {branches = branches; visited = additional_visited}
+
+let space = '.'
+
+let findBranches (map: char [] []) (visited: (int * int) list) (position: int * int) 
     : Explore =
-    {branches = [||]; visited = [||]}
+    // TO-DO
+    {branches = [||]; visited = []}
 
-let rec buildTree (map: char [] []) (visited: (int * int)[]) (branch: Branch) : Tree =
+
+(*
+
+#########
+#b.A.@.a#
+#########
+So, collecting every key took a total of 8 steps.
+
+
+
+Here is a larger example:
+
+########################
+#f.D.E.e.C.b.A.@.a.B.c.#
+######################.#
+#d.....................#
+########################
+
+Finally, collect key e to unlock door E, then collect key f, taking a grand total of 86 steps.
+
+Here are a few more examples:
+
+########################
+#...............b.C.D.f#
+#.######################
+#.....@.a.B.c.d.A.e.F.g#
+########################
+Shortest path is 132 steps: b, a, c, d, f, e, g
+
+#################
+#i.G..c...e..H.p#
+########.########
+#j.A..b...f..D.o#
+########@########
+#k.E..a...g..B.n#
+########.########
+#l.F..d...h..C.m#
+#################
+Shortest paths are 136 steps;
+one is: a, f, b, j, g, n, h, d, l, o, e, p, c, i, k, m
+
+########################
+#@..............ac.GI.b#
+###d#e#f################
+###A#B#C################
+###g#h#i################
+########################
+Shortest paths are 81 steps; one is: a, c, f, i, d, g, b, e, h
+
+*)
+
+
+let rec buildTree (map: char [] []) (visited: (int * int) list) (branch: Branch) : Tree =
     let expl = findBranches map visited branch.position
     if  (expl.branches = [||]) 
     then {area = branch.area; distance = branch.distance; branches = [||]; back = None } else
@@ -54,7 +135,6 @@ let otherBranches (i:int) (solution: Solution) : Tree[] =
         {branches.[other] with distance = distance +  branches.[i].distance}
     )
 
-let space = '.'
 type Step = | KeyStep | SpaceStep
 
 let next (step:Step) (i:int) (solution: Solution) : Solution =
@@ -108,7 +188,7 @@ let main _ =
     let sw = Stopwatch()
     sw.Start()
 
-    let map = parse @"C:\dev\FSharp\AoC2019\Day18\input.txt"
+    let map = parse @"C:\dev\FSharp\AoC2019\Day18\input_demo.txt"
 
     printfn "executed in %d ms"  sw.ElapsedMilliseconds
     Console.ReadKey() |> ignore
