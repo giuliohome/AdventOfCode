d7283f8b6c1fa9fae79bc8c70b582e23270addda
giuliohome <giuliohome@gmail.com>
2020-12-03 18:17:44 +0100

    generic down slope

--- AoC2020/Solver/Day3.fs
+++ AoC2020/Solver/Day3.fs
@@ -15,8 +15,8 @@ let rows = lines.Length
 
 type Move = { right:int; down:int}
 let move1 = {right=3; down=1}
-type State = {result:int; xpos:int}
-let start ={result=0; xpos=0}
+type State = {result:int; xpos:int; ypos:int; curr_row:int}
+let start ={result=0; xpos=0; ypos=0; curr_row=0}
 
 let step1 (debug:bool) (state:State) (nums:'a) =
     if debug 
@@ -29,12 +29,16 @@ let step1 (debug:bool) (state:State) (nums:'a) =
             |_ -> printf ".")
             nums
         printfn ""
+    if state.curr_row < state.ypos
+    then {state with curr_row = state.curr_row + 1}
+    else 
     let result =
         if (nums: bool[]).[state.xpos]
         then state.result + 1
         else state.result
     let xpos = (state.xpos + move1.right) % cols
-    {result=result; xpos=xpos}
+    let ypos = (state.ypos + move1.down) % rows
+    {result=result; xpos=xpos; ypos=ypos; curr_row=state.curr_row + 1}
 
 let step2 (counter:'s) (nums:'a) =
 
