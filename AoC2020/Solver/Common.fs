57fca037c7dff3de1e515faa63f55e28cec88328
giuliohome <giuliohome@gmail.com>
2020-12-05 10:13:54 +0100

    separation of concerns

--- AoC2020/Solver/Common.fs
+++ AoC2020/Solver/Common.fs
@@ -26,3 +26,15 @@ let debugger liner step start lines=
     generator  analyse liner step start lines true
 let solver  liner step start lines =
     generator  analyse liner step start lines false
+
+
+type State = {result:int; xpos:int; ypos:int; curr_row:int; curr_list: int list}
+let start ={result=0; xpos=0; ypos=0; curr_row=0; curr_list = []}
+
+let phase step move liner (lines: string[]) =
+    let cols = lines.[0].Length
+    let rows = lines.Length
+    let s = 
+       solver
+           liner (step cols rows move) start lines 
+    s.curr_list
--- AoC2020/Solver/Day5.fs
+++ AoC2020/Solver/Day5.fs
@@ -9,8 +9,6 @@ let liner (line:string)  = line.ToCharArray()
 let firstLineTest = liner lines.[0]
 
 let move1 = [|[|0..127|]; [|0..8|]|]
-type State = {result:int; xpos:int; ypos:int; curr_row:int; curr_list: int list}
-let start ={result=0; xpos=0; ypos=0; curr_row=0; curr_list = []}
 
 let step1 cols rows move (state:State) nums =
     let read f take _skip array  = 
@@ -39,25 +37,18 @@ let step1 cols rows move (state:State) nums =
     
     {state with
         curr_list = curr_list; 
-        curr_row=state.curr_row + 1}
+        curr_row=state.curr_row + 1}   
+
+
+let phase1 (lines: string[]) =
+    phase step1 move1 liner lines
+    |> List.max
 
 let move2 = move1
 let step2 = step1
-    
-let phase (lines: string[]) =
-    let cols = lines.[0].Length
-    let rows = lines.Length
-    let s = 
-       solver
-           liner (step1 cols rows move1) start lines 
-    s.curr_list
-
-let phase1 (lines: string[]) =
-    phase lines
-    |> List.max
 
 let phase2  (lines: string[]) =
-    let curr_list = phase lines 
+    let curr_list = phase step2 move2 liner lines 
     let my =
         [curr_list |> List.min .. curr_list |> List.max]
         |> List.filter (fun x -> 
