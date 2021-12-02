af87730db4a826b957acb322538bd0d9ae43ad5b
Giulio <giuliohome@gmail.com>
2021-12-02 14:51:50 +0100

    2021 day 2

---
+++ AoC2021/day02/solve.fsx
@@ -0,0 +1,56 @@
+open System
+open System.IO
+
+let sr = new StreamReader("input.txt")
+
+let lines =
+    [|
+    while (not sr.EndOfStream) do
+        yield sr.ReadLine() 
+    |]
+
+type Move =
+| Forward of int
+| Up of int
+| Down of int
+| Error of string
+
+let moves = 
+  lines
+  |> Array.map(fun l -> 
+    let split = l.Split(" ") 
+    let cmd = split.[0]
+    let num = int (split.[1]) 
+    match cmd with
+    | "forward" -> Forward num
+    | "up" -> Up num
+    | "down" -> Down num
+    | _ -> Error l
+  )
+
+let hor, ver = 
+  moves
+  |> Array.fold 
+    (fun (hor, ver) line  ->
+      match line with
+      | Forward f -> (hor + f, ver)
+      | Up u -> (hor, ver - u)
+      | Down d -> (hor, ver + d)
+    )
+    (0,0)
+
+printfn "part 1 %d" (hor *  ver)
+
+let aim, hor2, ver2 = 
+  moves
+  |> Array.fold 
+    (fun (aim, hor, ver) line  ->
+      match line with
+      | Forward f -> (aim, hor + f, ver + aim * f)
+      | Up u -> (aim - u, hor, ver)
+      | Down d -> (aim + d, hor, ver)
+    )
+    (0,0,0)
+
+printfn "part 2 %d" (hor2 *  ver2)
+
